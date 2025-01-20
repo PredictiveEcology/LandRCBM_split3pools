768d1b43c982ee63cc7c2118c398bcbe13a4529b
Dominique Caron <dominique.caron@mail.mcgill.ca>
2025-01-20 15:18:10 -0800

    reducing example FAIL

--- global.R
+++ global.R
@@ -6,7 +6,9 @@ if (!require("SpaDES.project")){
 
 ## Objects
 CBM_AGB <- reproducible::prepInputs("data/yield_module_sample/CBM_AGB.csv", fun = "data.table::fread")
+CBM_AGB <- CBM_AGB[CBM_AGB$pixelGroup %in% c(1,2,3),]
 CBM_speciesCodes <- reproducible::prepInputs("data/yield_module_sample/CBM_speciesCodes.csv", fun = "data.table::fread")
+CBM_speciesCodes <- CBM_speciesCodes[CBM_speciesCodes$pixelGroup %in% c(1,2,3),]
 pixelGroupMap <- reproducible::prepInputs("data/yield_module_sample/pixelGroupMap.tiff", fun = "terra::rast")
 rasterToMatch = pixelGroupMap
 
@@ -26,7 +28,7 @@ split3poolsInit <- SpaDES.core::simInit(
   params = parameters,
   modules = modules,
   objects = objects,
-  paths = "~/Repos/"
+  paths = list(modulePath = "~/Repos/")
   )
 
 split3poolsSim <- spades(split3poolsInit)
