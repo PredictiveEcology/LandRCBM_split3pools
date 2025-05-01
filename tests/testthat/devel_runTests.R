d5fe774eeb8d17f670d3886d02ca54ffe1efa252
DominiqueCaron <dominique.caron@mail.mcgill.ca>
2025-05-01 14:12:47 -0700

    test are passing

--- tests/testthat/devel_runTests.R
+++ tests/testthat/devel_runTests.R
@@ -1,19 +1,23 @@
 ## SET UP ----
 
 # Install required packages
-# Get needed packages
-packages = c("PredictiveEcology/SpaDES.core", "reproducible (>= 2.1.2)", "data.table", "ggplot2", "terra",
-             "SpaDES.tools", "PredictiveEcology/CBMutils@development", "PredictiveEcology/LandR@development")
-if (!requireNamespace("Require", quietly = TRUE)) {
-  install.packages("Require")
-}
-Require::Require(packages, dependencies = TRUE, repos = unique(c("predictiveecology.r-universe.dev", getOption("repos"))))
+## Required because module is not an R package
+install.packages(c("testthat", "SpaDES.core", "SpaDES.project"), repos = unique(c(
+  "predictiveecology.r-universe.dev", getOption("repos")
+)))
+
+Require::Require(c("data.table", "terra", "LandR"))
+
+## OPTIONS ----
 
 # Suppress warnings from calls to setupProject, simInit, and spades
 options("spades.test.suppressWarnings" = TRUE)
 
-# Set custom input data location
-options("reproducible.inputPaths" = NULL)
+# Set custom directory paths
+## Speed up tests by allowing inputs, cache, and R packages to persist between runs
+options("spades.test.paths.inputs"   = NULL) # inputPath
+options("spades.test.paths.cache"    = NULL) # cachePath
+options("spades.test.paths.packages" = NULL) # packagePath
 
 ## RUN ALL TESTS ----
 # Run all tests
