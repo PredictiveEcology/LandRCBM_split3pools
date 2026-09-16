7e2277e0c6d7cec1e6c4c146016e515355b7ee08
Susan Murray <murray.e.susan@gmail.com>
2026-09-16 11:26:04 -0700

    test_disturbance module inputs updated

--- tests/testthat/testdata/modules/test_disturbance/test_disturbance.R
+++ tests/testthat/testdata/modules/test_disturbance/test_disturbance.R
@@ -5,7 +5,8 @@ defineModule(sim, list(
   timeunit = "year",
   reqdPkgs = list("data.table"),
   inputObjects = bindrows(
-    expectsInput(objectName = "cohortData", objectClass = "data.table", desc = NA, sourceURL = NA)
+    expectsInput(objectName = "cohortData", objectClass = "data.table", desc = NA, sourceURL = NA),
+    expectsInput(objectName = "disturbanceEvents", objectClass = "data.table", desc = NA, sourceURL = NA)
   ),
   outputObjects = bindrows(
     createsOutput(objectName = "cohortData", objectClass = "data.table", desc = NA)
